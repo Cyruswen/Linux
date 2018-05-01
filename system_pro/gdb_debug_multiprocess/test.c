48f8fde32bae160f5d6c5e1e20268fdd3140cf25
Cyruswen <Cyrus_wen@163.com>
2018-05-01 16:47:46 +0800

    add gdb

---
+++ system_pro/gdb_debug_multiprocess/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void processA(){
+    printf("I am the child process,%d!\n",getpid());
+    return;
+}
+
+void processB(){
+    printf("I am the parent process,%d!\n",getpid());
+}
+
+int main(){
+    pid_t pid;
+    pid = fork();
+    if(pid < 0){
+        perror("fork");
+        exit(-1);
+    }else if(pid == 0){
+        processA();
+        exit(0);
+    }else{
+        processB();
+        sleep(1);
+    }
+    return 0;
+}
